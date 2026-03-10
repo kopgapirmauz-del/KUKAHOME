63a2afeb9a8bdd00d733387e92a62e4ce049547a
kopgapirmauz-del <kopgapirmauz@gmail.com>
2026-03-10 15:52:51 +0500

    Add files via upload

---
+++ assets/data/products-manifest.js
@@ -0,0 +1,24 @@
+window.KUKA_PRODUCTS_MANIFEST = [
+  {
+    "id": "1",
+    "folder": "1.6033",
+    "model": "6033",
+    "info": "Yangi modelimiz juda zor bu. Tavfsiya qilamiz!",
+    "images": [
+      "assets/images/products/mebel/1.6033/product-1.jpg",
+      "assets/images/products/mebel/1.6033/product-12.jpg",
+      "assets/images/products/mebel/1.6033/product-123.jpg"
+    ]
+  },
+  {
+    "id": "2",
+    "folder": "2.BY.736B",
+    "model": "BY.736B",
+    "info": "Yangi modelimiz juda zor bu. Tavfsiya qilamiz!",
+    "images": [
+      "assets/images/products/mebel/2.BY.736B/product-2.jpg",
+      "assets/images/products/mebel/2.BY.736B/product-21.jpg",
+      "assets/images/products/mebel/2.BY.736B/product-212.jpg"
+    ]
+  }
+];
